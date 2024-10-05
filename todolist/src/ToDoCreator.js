5270056eb095fe5c61717aab4949ea2ac9ca7c50
Syed Saifuddin <saifuddinsyed60@gmail.com>
2024-10-05 12:09:49 -0500

    Added css for input form

--- todolist/src/ToDoCreator.js
+++ todolist/src/ToDoCreator.js
@@ -1,26 +1,28 @@
 import { Form,Button, Row, Col  } from "react-bootstrap";
-
+import {ToDoCreator1} from './ToDoCreator.css'
 function ToDoCreator() {
     return (
+        <div class="form1">
         <Form>
             <Row className="mb-3">
                 <Form.Group className="mb-3" controlId="ToDoItemInput">
                     <Form.Label>ToDo Item</Form.Label>
-                    <Form.Control placeholder="Add todo item" />
+                    <Form.Control className="w-85" placeholder="Add todo item" />
                 </Form.Group>
             </Row>
 
             <Row className="mb-3">
                 <Form.Group controlId="dueDateInput">
-                    <Form.Label>Due Date</Form.Label>
-                    <input type="date" id="dueDateField" />                 
+                    <Form.Label>Due Date</Form.Label> <br/>
+                    <Form.Control type="date" className="w-85" id="dueDateField" />            
                 </Form.Group>
             </Row>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" className="w-100" type="submit">
                 Add Todo
             </Button>
         </Form>
+        </div>
     );
 }
 export default ToDoCreator;
