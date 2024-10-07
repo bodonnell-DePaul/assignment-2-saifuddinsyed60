f371fb9eb9d47395fa86c4ade33a48ec10d40549
Syed Saifuddin <saifuddinsyed60@gmail.com>
2024-10-07 01:44:38 -0500

    Change

--- todolist/src/ToDoCreator.test.js
+++ todolist/src/ToDoCreator.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from './TodoList';
-import ToDoCreator from './TodoCreator';
+import TodoCreator from './TodoCreator';
 import ViewPort from './ViewPort';
 
 describe('TodoList Component', () => {
@@ -10,7 +10,7 @@ describe('TodoList Component', () => {
   
 
   test('renders form elements', () => {
-    render(<ToDoCreator />);
+    render(<TodoCreator />);
     const todoInput = screen.getByPlaceholderText(/Add todo item/i);
     const dueDateInput = screen.getByLabelText(/Due Date/i);
     const addButton = screen.getByText(/Add Todo/i);
